7eadb88c56d22cf73bb38e2553cc1c38bf2a2bc6
edmond <edma_wu@yahoo.com>
2016-11-01 16:48:13 -0400

    rle begin implementation

--- compressT_LOLS.c
+++ compressT_LOLS.c
@@ -3,16 +3,22 @@
 #include <string.h>
 
 char* compress(char *string) {
-
+	int length = strlen(string);
+	char *compressed = (char *)malloc(sizeof(char) * (length * 2 + 1));
+	for (int i = 0; i < length; i++) {
+		
+	}
+	return compressed;
 }
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
+	if (argc != 2) {
 		fprintf(stderr, "ERROR: Invalid number of arguments; only 1 argument required\n");
 	}
 	else {
-    char *compressed = compress(argv[1]);
+    		char *compressed = compress(argv[1]);
 		printf("Compressed string: %s\n", compressed);
+		free(compressed);
 	}
-  return 0;
+	return 0;
 }
