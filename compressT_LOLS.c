5aeb35f33e71855a5e1b5a2a46b47037e41b8078
edmond <edma_wu@yahoo.com>
2016-11-08 00:34:00 -0500

    added some comments

--- compressT_LOLS.c
+++ compressT_LOLS.c
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 #include <pthread.h> /*needs -pthread compilation option*/
 #include "functions.h"
+#define NUM_THREADS 5
 
 void process_file(FILE *file) {
 	if (file == NULL)
 		fprintf(stderr, "Invalid file input\n");
 	else {
+		//open file and store contents in string
 		fseek(file, 0, SEEK_END);
 		size_t length = ftell(file);
 		rewind(file);
 		char *buffer = (char *)malloc(length + 1);
 		buffer[length] = '\0';
 		fread(buffer, 1, length, file);
+
+		//compress string and write to new file
 		char *compressed = compress(buffer);
 		printf("Compressed string: %s\n", compressed);
 		write_file("test_txt_LOLS", compressed);
