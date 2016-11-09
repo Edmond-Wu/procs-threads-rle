4ffa95e05e54cd8a219ef72823123df5fa8fa796
edmond <edma_wu@yahoo.com>
2016-11-08 21:35:09 -0500

    testing multithreading

--- compressT_LOLS.c
+++ compressT_LOLS.c
@@ -9,13 +9,13 @@ void thread_function(Args *args) {
 	printf("Compressed: %s\n", str);
 	int file_name_length = strlen(args->file_name);
 	char *new_file_name = (char *)malloc(sizeof(char) * (file_name_length + 7));
-	if (args->part > 1)
-		sprintf(new_file_name, "test_%s_LOLS%d", get_file_extension(args->file_name), args->part);
-	else
-		sprintf(new_file_name, "test_%s_LOLS", get_file_extension(args->file_name));
+	//test.txt -> test_txt_LOLS
+	strncpy(new_file_name,args->file_name,file_name_length - strlen(strpbrk(args->file_name,".")));
+	sprintf(new_file_name, "%s_%s_LOLS%d", new_file_name, get_file_extension(args->file_name), args->part);
 	write_file(new_file_name, str);
 	free(new_file_name);
 	free(str);
+	free(args);
 	return NULL;
 }
 
@@ -29,16 +29,9 @@ void process_file(char *file_name, FILE *file, int parts) {
 
 		char **array = split_string(buffer, parts);
 		pthread_t threads[parts];
-		/*
-		for (int x = 0; x < parts; x++)
-			printf("Substring: %s\n", array[x]);
-		for (int i = 0; i < parts; i++)
-			printf("Compressed: %s\n", compress(array[i]));
-		*/
-		
 		for (int i = 0; i < parts; i++) {
 			Args *args = (Args *)malloc(sizeof(Args));
-			args->part = i + 1;
+			args->part = i;
 			args->string = array[i];
 			args->file_name = file_name;
 			pthread_create(&threads[i], NULL, thread_function, args);
